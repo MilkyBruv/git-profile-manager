f7ddb2df3151256bdaa06ed58e26f81b05f95f5a
MilkyBruv <malachi.mcenroe@gmail.com>
2026-03-19 15:27:26 +1300

    created prototype python, works super well trust

--- gpm.c
+++ gpm.c
@@ -2,79 +2,23 @@
 #include <stdlib.h>
 #include <direct.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct profile
 {
-    char* name, email;
+    char* alias;
+    char* name;
+    char* email;
 } profile_t;
 
 int main(int argc, const char* argv[])
 {
-    for (size_t i = 0; i < argc; i++)
+    FILE* file = fopen("./profiles.ini", "r");
+    char buffer[128];
+
+    while (fgets(buffer, 128, file))
     {
-        printf("%llu: %s, ", i, argv[i]);
-    }
-    printf("\n");
-
-    printf("%i\n", argc);
-    const char* mode = argv[1];
-
-    // gpm status
-    // gpm switch <alias>
-
-    if (argc != 1)
-    {
-        if (strcmp(mode, "status") == 0)
-        {
-            printf("\x1b[32mUsername:\x1b[39m\n");
-            system("git config get user.name");
-            printf("\x1b[32mEmail:\x1b[39m\n");
-            system("git config get user.email");
-        }
-        else if (strcmp(mode, "list") == 0)
-        {
-            char total[] = "";
-
-            FILE* file = fopen("./profiles.conf", "r");
-            char buffer[128];
-
-            while (fgets(buffer, 128, file))
-            {
-                strcat(total, buffer);
-                char* name = "NONE";
-                char* email = "NONE";
-
-                char* token = strtok(buffer, " = ");
-                char* prev_token = "";
-                while (token != NULL)
-                {
-                    if (strchr(token, '\"') != NULL || strchr(token, '\'') != NULL)
-                    {
-                        if (strcmp(prev_token, "name"))
-                        {
-                            name = token;
-                            printf("found: %s\n", name);
-                        }
-                        else if (strcmp(prev_token, "email"))
-                        {
-                            email = token;
-                            printf("found: %s\n", email);
-                        }
-                    }
-
-                    *prev_token = *token;
-                    token = strtok(NULL, " = ");
-                }
-            
-                printf("name: %s\nemail: %s\n", name, email);
-            }
-
-            fclose(file);
-        }
-        else
-        {
-            printf("aint a command mayte\n");
-        }
+        printf("%s\n", buffer);
     }
 
     return 0;
---
+++ proto.py
@@ -0,0 +1,74 @@
+import sys
+from os import system, popen
+
+class Profile():
+    alias = None
+    name = None
+    email = None
+    def __init__(self, alias, name, email):
+        self.alias = alias
+        self.name = name
+        self.email = email
+    
+    def print_info(self):
+        print(f"{self.alias}, {self.name}, {self.email}")
+
+with open("./profiles.ini", "r") as file:
+    lines = file.readlines()
+
+# print(lines)
+
+profiles = []
+
+new_alias = None
+new_name = None
+new_email = None
+current_profile = Profile(None, None, None)
+
+for line in lines:
+    line = line.strip()
+
+    if (len(line) < 2):
+        continue
+
+    if new_alias == None and line[0] == "[":
+        new_alias = line.replace("[", "").replace("]", "").replace("\n", "")
+        
+    if new_name == None and line.split("=")[0].strip() == "name":
+        new_name = line.split("=")[1].strip().replace("\"", "").replace("\'", "")
+
+    if new_email == None and line.split("=")[0].strip() == "email":
+        new_email = line.split("=")[1].strip().replace("\"", "").replace("\'", "")
+
+    if new_alias != None and new_name != None and new_email != None:
+        profiles.append(Profile(new_alias, new_name, new_email))
+        new_alias = None
+        new_name = None
+        new_email = None
+
+args = sys.argv
+name = popen("git config get user.name").read().strip()
+email = popen("git config get user.email").read().strip()
+
+current_profile = None
+for profile in profiles:
+    if current_profile == None and profile.name == name and profile.email == email:
+        current_profile = profile.alias
+
+if args[1] == "status":
+    for profile in profiles:
+        if profile.name == name and profile.email == email:
+            print(f"Profile: {profile.alias}\nUsername: {name}\nEmail: {email}");
+
+elif args[1] == "switch":
+    for profile in profiles:
+        if profile.alias == args[2] and current_profile != args[2]:
+            system(f"git config --global user.name \"{profile.name}\"")
+            system(f"git config --global user.email \"{profile.email}\"")
+            print(f"Switched from {current_profile} to {profile.alias}")
+            exit(0)
+
+    print(f"Already on {current_profile}")
+    exit(0)
+
+        
